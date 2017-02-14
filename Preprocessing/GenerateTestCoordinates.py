4285561a4e1c1709a4f5f100c65070859867c4db
swilso421 <swilso421@gmail.com>
2017-02-13 23:44:19 -0500

    Changed point generation method to something smoother

--- Preprocessing/GenerateTestCoordinates.py
+++ Preprocessing/GenerateTestCoordinates.py
@@ -4,30 +4,43 @@ from random import *
 
 print("Script for generating two dimensional coordinate data")
 print("Data is stored in a txt file where each line has a pair of coordinates, delimited by a colon")
-print("Max distance in each dimension from previous point is 0.05")
+#print("Max distance in each dimension from previous point is 0.05")
 print("Domain of each dimension is (-5.0, 5.0)")
-print("WARNING!: This script does not validate input or handle errors")
+#print("WARNING!: This script does not validate input or handle errors")
 
-filename = raw_input("Name of data file: ")
-n = int(raw_input("Number of coordinate pairs to generate: "))
+#filename = raw_input("Name of data file: ")
+#n = int(raw_input("Number of coordinate pairs to generate: "))
+
+n = 200
 
 x = 0.0
 z = 0.0
 
-with open("{}.txt".format(filename), "w+") as f:
+def distance(p1, p2):
+    return sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+
+with open("test_data.txt", "w+") as f:
 
     for i in range(n):
 
-        x = x + (0.1 * random() - 0.05)
-        z = z + (0.1 * random() - 0.05)
+        newx = 10.0 * random() - 5.0
+        newz = 10.0 * random() - 5.0
 
-        if x > 5.0:
-            x = 5.0
-        elif x < -5.0:
-            x = -5.0
-        if z > 5.0:
-            z = 5.0
-        elif z < -5.0:
-            z = -5.0
+        if newx > 5.0:
+            newx = 5.0
+        elif newx < -5.0:
+            newx = -5.0
+        if newz > 5.0:
+            newz = 5.0
+        elif newz < -5.0:
+            newz = -5.0
 
-        f.write("{0}:{1}\n".format(x, z))
+        deltax = (newx - x) / 100.0
+        deltaz = (newz - z) / 100.0
+
+        for j in range(100):
+
+            f.write("{0}:{1}\n".format(x + (j + 1) * deltax, z + (j + 1) *deltaz))
+
+        x = newx
+        z = newz
