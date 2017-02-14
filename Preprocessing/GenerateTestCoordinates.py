963db884dd450665c558f0c14faee68ece01daf9
swilso421 <swilso421@gmail.com>
2017-02-13 23:19:49 -0500

    Updated scale

--- Preprocessing/GenerateTestCoordinates.py
+++ Preprocessing/GenerateTestCoordinates.py
@@ -4,7 +4,7 @@ from random import *
 
 print("Script for generating two dimensional coordinate data")
 print("Data is stored in a txt file where each line has a pair of coordinates, delimited by a colon")
-print("Max distance in each dimension from previous point is 0.1")
+print("Max distance in each dimension from previous point is 0.05")
 print("Domain of each dimension is (-5.0, 5.0)")
 print("WARNING!: This script does not validate input or handle errors")
 
@@ -18,8 +18,8 @@ with open("{}.txt".format(filename), "w+") as f:
 
     for i in range(n):
 
-        x = x + (0.2 * random() - 0.1)
-        z = z + (0.2 * random() - 0.1)
+        x = x + (0.1 * random() - 0.05)
+        z = z + (0.1 * random() - 0.05)
 
         if x > 5.0:
             x = 5.0
