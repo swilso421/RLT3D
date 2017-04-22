f59f68d94556584fb49768bef6287ebe3cd2877e
Sam Wilson <swilso421@gmail.com>
2017-04-22 12:08:33 -0400

    Added comments

--- Blender/WayfairAPI.py
+++ Blender/WayfairAPI.py
@@ -35,6 +35,9 @@ def getJSON(response):
     except:
         return False, None
 
+#Downloads a specific model from a url. Technically, this is just a generic
+#download function and could download any file type
+#Also, this will overwrite any file with the same filepath
 def downloadModelFromURL(filepath, url, stream = False):
 
     #Get a response object from the target URL
@@ -60,43 +63,56 @@ def fetchModel(sku, directory):
     #Prepare the filepath that will be used later
     filepath = os.path.join(directory, '{}.fbx'.format(sku))
 
+    #Gets all the download urls for the target model
     response = requests.get(url, headers = HEADERS)
 
+    #Attempts to read the models + urls table. If successful = false; the script
+    #was probably flagged as a bot
     successful, data = getJSON(response)
 
     if not successful:
         return False, 'bad response'
 
+    #If the model doesn't have an fbx download, return false
     if not 'fbx' in data[sku]:
         return False, 'Model {} does not have an fbx download'.format(sku)
 
-    downloadModelFromURL(filepath, data[sku]['fbx'])
+    #Returns the result of the download; i.e. whether or not it was downloaded
+    return downloadModelFromURL(filepath, data[sku]['fbx'])
 
 #OUTDATED: Requires rewrite to utilize fetchModel()
 #Downloads ALL fbx model files from the Wayfair database. This operation will take a couple HOURS to do
 def downloadAllModels(directory):
 
+    #Concatenates the appropriate url
     url = BASE_URL + MODEL_ENDPOINT + ALL_PAGES_TAG
 
     print('Requesting model URLs...')
 
+    #Gets the list of all models by sku and all of their downloads
     response = requests.get(url, headers = HEADERS)
 
     print('Converting response to JSON...')
 
+    #Attempts to read the models + urls table. If successful = false; the script
+    #was probably flagged as a bot
     successful, data = getJSON(response)
 
     if not successful:
         return False, 'bad response'
 
+    #Instantiates a dictionary for the download urls
     modelURLs = {}
 
     print('Parsing model URLs...')
 
+    #Out of all the models found, we only care about models with fbx files.
+    #This grabs only the fbx urls into our dictionary
     for entry in data:
         if 'fbx' in data[entry]:
             modelURLs[entry] = data[entry]['fbx']
 
+    #Clear these variables since we wil reuse them
     del response, data
 
     modelCount = len(modelURLs)
@@ -105,8 +121,10 @@ def downloadAllModels(directory):
 
     for model in modelURLs:
 
+        #Concatenates the appropriate filepath for the model to be downloaded
         filepath = os.path.join(directory, '{}.fbx'.format(model))
 
+        #If the model isn't already downloaded, download it
         if not os.path.isfile(filepath):
             print('Downloading model {sku} to {path}'.format(sku = model, path = filepath))
             downloadModelFromURL(filepath, modelURLs[model])
